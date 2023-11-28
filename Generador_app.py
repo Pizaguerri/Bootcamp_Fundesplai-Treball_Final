b06c7db678a261d391b0d3dcbd2dcf0ec98ecdd5
Pizaguerri <p.izaguerri@gmail.com>
2023-11-28 19:33:25 +0100

    generador con botones

--- Generador_app.py
+++ Generador_app.py
@@ -15,8 +15,7 @@ color_terciario = "#f48d24"
 #Precio Base o unitario por defecto (pb)
 def activar_pb(list_n):
     pb_lists = [concepto_opciones]
-    contador_pb_lists = [concepto_contador]
-
+    contador_pb_lists = [concepto_contador_ent_1, concepto_contador_ent_2, concepto_contador_ent_3]
     for contador_pb in range(0,len(pb_lists[list_n])):
         if pb_lists[list_n][contador_pb].get() == 1:
             contador_pb_lists[list_n][contador_pb].configure(state=NORMAL)
@@ -25,15 +24,14 @@ def activar_pb(list_n):
 
 
 #Valor horas trabajadas (h)
-def activar_h(list_n):
-    h_lists = [[concepto_opciones]]
-    contador_h_lists = [concepto_contador]
-
-    for contador_h in range(0,len(h_lists[list_n])):
-        if h_lists[list_n][contador_h].get() == 1:
-            contador_h_lists[list_n][contador_h].configure(state=NORMAL)
-        else:
-            contador_h_lists[list_n][contador_h].configure(state=DISABLED)
+# def activar_h(list_n):
+#     h_lists = [[concepto_opciones]]
+#     contador_h_lists = [concepto_contador_ent_1, concepto_contador_ent_2, concepto_contador_ent_3]
+#     for contador_h in range(0,len(h_lists[list_n])):
+#         if h_lists[list_n][contador_h].get() == 1:
+#             contador_h_lists[list_n][contador_h].configure(state=NORMAL)
+#         else:
+#             contador_h_lists[list_n][contador_h].configure(state=DISABLED)
 
 
 #Ventana Principal
@@ -73,20 +71,22 @@ totales_list = ["Subtotal", "Total"]
 
 #Checkbuttons de los conceptos con caja de entrada de valores
 concepto_opciones = list()
-concepto_contador = list()
+concepto_contador_ent_1 = list()
+concepto_contador_ent_2 = list()
+concepto_contador_ent_3 = list()
 contador = 0
 for concepto in concepto_list:
     concepto_opciones.append(IntVar())
     #cajas_texto = Checkbutton(concepto_frame_lbl, text=concepto, bg=color_claro, fg=color_primario, font=("Helvetica",18), onvalue=1, offvalue=0, variable=concepto_opciones[contador], command=lambda:activar_pb(0))
-    cajas_texto = Checkbutton(concepto_frame_lbl, text=concepto, bg=color_claro, fg=color_primario, font=("Helvetica",18), variable=concepto_opciones[contador], command=lambda:activar_pb(0))
-    cajas_texto.grid(row=contador,column=0,sticky=W)
-    concepto_contador.append(Entry(concepto_frame_lbl, font=("Helvetica",18), justify=CENTER, bd=1, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
-    concepto_contador[contador].grid(row=contador,column=1,sticky=W)
+    cajas_texto = Checkbutton(concepto_frame_lbl, text=concepto, bg=color_claro, fg=color_primario, font=("Helvetica",18), relief=FLAT, variable=concepto_opciones[contador], command=lambda:activar_pb(0))
+    cajas_texto.grid(row=contador,column=0,sticky=W, padx=5, pady=5)
+    concepto_contador_ent_1.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=1, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
+    concepto_contador_ent_1[contador].grid(row=contador,column=1,sticky=W)
     #2 cajas de entry extra
-    # concepto_contador.append(Entry(concepto_frame_lbl, font=("Helvetica",18), justify=CENTER, bd=1, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
-    # concepto_contador[contador].grid(row=contador,column=contador,sticky=W)
-    # concepto_contador.append(Entry(concepto_frame_lbl, font=("Helvetica",18), justify=CENTER, bd=1, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
-    # concepto_contador[contador].grid(row=contador,column=3,sticky=W)
+    concepto_contador_ent_2.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=1, width=5, state=DISABLED, fg=color_primario, bg=color_claro))
+    concepto_contador_ent_2[contador].grid(row=contador,column=2,sticky=W)
+    concepto_contador_ent_3.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=1, width=5, state=DISABLED, fg=color_primario, bg=color_claro))
+    concepto_contador_ent_3[contador].grid(row=contador,column=3,sticky=W)
     contador += 1
 
 
@@ -96,7 +96,7 @@ botones_contador = list()
 contador = 0
 for botones in botones_list:
     botones_opciones.append(IntVar())
-    botones = Button(botones_frame_lbl, text=botones, font=("Helvetica",18), bd=0, padx=2, pady=2, width=16, relief=FLAT, bg=color_claro, fg=color_primario)
+    botones = Button(botones_frame_lbl, text=botones, font=("Helvetica",18), bd=0, padx=5, pady=5, width=36, relief=FLAT, bg=color_claro, fg=color_primario)
     botones.grid(row=contador+1, column=0, sticky=W)
     contador += 1
 
