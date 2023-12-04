5c92d92210cc5a775e903a5f92d703a9015ed8d9
Pizaguerri <p.izaguerri@gmail.com>
2023-12-04 19:30:22 +0100

    "Calculadora funciona"

--- porsupuestapp.py
+++ porsupuestapp.py
@@ -1,5 +1,5 @@
 """ Generador de Presupuestos """
-import os
+# import os
 from tkinter import *
 
 color_primario = "#373737"
@@ -9,26 +9,48 @@ color_terciario = "#f48d24"
 gris_claro = "#ecedec"
 
 #Precio Base o unitario por defecto (pb)
-def activar_textos(list_n):
-    texto_lists = [concepto_opciones]
-    contador_textos_lists = [concepto_contador_ent_1, concepto_contador_ent_2, concepto_contador_ent_3]
-    for contador_textos in range(0,len(texto_lists[list_n])):
-        if texto_lists[list_n][contador_textos].get() == 1:
-            contador_textos_lists[0][contador_textos].configure(state=NORMAL)
-            contador_textos_lists[1][contador_textos].configure(state=NORMAL)
-            contador_textos_lists[2][contador_textos].configure(disabledbackground=color_claro, disabledforeground=color_primario)
-        else:
-            contador_textos_lists[0][contador_textos].configure(state=DISABLED)
-            contador_textos_lists[1][contador_textos].configure(state=DISABLED)
-            contador_textos_lists[2][contador_textos].configure(disabledbackground=gris_claro, disabledforeground=color_primario)
+def activar_textos(cont):
+    if concepto_opciones[cont].get() == 1:
+        concepto_contador_ent_1[cont].config(state=NORMAL)
+        concepto_contador_ent_2[cont].config(state=NORMAL)
+        concepto_contador_ent_3[cont].configure(disabledbackground=color_claro, disabledforeground=color_primario)
+    else:
+        concepto_contador_ent_1[cont].delete(0,END)
+        concepto_contador_ent_1[cont].config(state=DISABLED)
+        entry2_vars[cont].set("")
+        concepto_contador_ent_2[cont].config(state=DISABLED)
+        entry3_vars[cont].set(0.00)
+        concepto_contador_ent_3[cont].configure(disabledbackground=gris_claro, disabledforeground=color_primario)
 
+def activa_impostos(imp):
+    if imp.lower() == 'iva':
+        if iva_var_cb.get() == 1:
+            iva_ent.configure(state=NORMAL)
+            iva_total_ent.config(disabledbackground=color_claro, disabledforeground=color_primario)
+        else:
+            iva_ent.delete(0,END)
+            iva_ent.configure(state=DISABLED)
+            iva_total_ent.configure(state=NORMAL)
+            iva_total_ent.delete(0,END)
+            iva_total_ent.configure(state=DISABLED)
+            iva_total_ent.config(disabledbackground=gris_claro, disabledforeground=color_primario)
+    elif imp.lower() == 'irpf':
+        if irpf_var_cb.get() == 1:
+            irpf_ent.configure(state=NORMAL)
+            irpf_total_ent.config(disabledbackground=color_claro, disabledforeground=color_primario)
+        else:
+            irpf_ent.delete(0,END)
+            irpf_ent.configure(state=DISABLED)
+            irpf_total_ent.config(state=NORMAL)
+            irpf_total_ent.delete(0,END)
+            irpf_total_ent.config(state=DISABLED)
+            irpf_total_ent.config(disabledbackground=gris_claro, disabledforeground=color_primario)
+            
 def calc_pressupost():
-    for entry in entrys_a_recoger:
-        print(entry.get())
+    pass
 
 def fer_factura():
-    for entry in entrys_a_recoger:
-        print(entry.get())
+    pass
 
 def historial():
     pass
@@ -36,17 +58,42 @@ def historial():
 def send_mail():
     pass
 
-def update_3rd_entry(var,cont):
-    if (var.get().replace('.', '').isnumeric() and concepto_contador_ent_1[cont].get().replace('.', '').isnumeric()):
-        concepto_contador_ent_3[cont].config(state=NORMAL)
-        concepto_contador_ent_3[cont].delete(0,END)
-        concepto_contador_ent_3[cont].insert(0,f"{float(concepto_contador_ent_1[cont].get())*float(var.get()):.2f}")
-        concepto_contador_ent_3[cont].config(state=DISABLED)
+def update_entry3(var,cont):
+    if var.get().replace('.', '').isnumeric() and concepto_contador_ent_1[cont].get().replace('.', '').isnumeric():
+        entry3_vars[cont].set(round(float(concepto_contador_ent_1[cont].get())*float(var.get()),2))
     else:
-        concepto_contador_ent_3[cont].config(state=NORMAL)
-        concepto_contador_ent_3[cont].delete(0,END)
-        concepto_contador_ent_3[cont].config(state=DISABLED)
-        print("Error en el canvi de tipus")
+        entry3_vars[cont].set(0.00)
+
+def update_impost(var,tipus):
+    if tipus.lower() == "iva":
+        if var.get().isnumeric():
+            if (subtotal_ent.get().replace('.', '').isnumeric()):
+                iva_total_ent.config(state=NORMAL)
+                iva_total_ent.delete(0,END)
+                iva_total_var.set(round(float(subtotal_ent.get())*int(var.get())/100,2))
+                iva_total_ent.config(state=DISABLED)
+    elif tipus.lower() == "irpf":
+        if var.get().isnumeric():
+            if (subtotal_ent.get().replace('.', '').isnumeric()):
+                irpf_total_ent.config(state=NORMAL)
+                irpf_total_ent.delete(0,END)
+                irpf_total_var.set(round(float(subtotal_ent.get())*int(var.get())/100,2))
+                irpf_total_ent.config(state=DISABLED)
+
+def update_subtotal():
+    subtotal_var.set(0.00)
+    suma = subtotal_var.get()
+    for entry_var in entry3_vars:
+        print(entry_var.get())
+        suma += entry_var.get()
+    subtotal_var.set(round(suma,2))
+
+def update_total():
+    total_var.set(0.00)
+    suma = subtotal_var.get()
+    suma += iva_total_var.get()
+    suma += irpf_total_var.get()
+    total_var.set(round(suma,2))
 
 # Comença programa
 main = Tk()
@@ -81,63 +128,86 @@ botones_frame.grid(row=1,column=0)
 botones_frame_lbl.pack(side=TOP)
 
 #Listas de conceptos y opciones
-concepto_list = ["Trabajo", "Extra", "Equipo", "Material", "Transporte", "Subtotal", "IVA", "IRPF", "Total"]
-# botones_list = ["Calcular Presupuesto", "Generar Factura", "Registro Histórico", "Enviar email"]
-totales_list = ["Subtotal", "Total"]
-impuestos_list = ["IVA, IRPF,"]
+concepto_list = ["Trabajo", "Extra", "Equipo", "Material", "Transporte"]
 
 #Checkbuttons de los conceptos con caja de entrada de valores
 concepto_opciones = list()
 concepto_contador_ent_1 = list()
 concepto_contador_ent_2 = list()
 concepto_contador_ent_3 = list()
-txt_vars = list()
-entrys_a_recoger = list()
-impuestos = list()
+entry2_vars = list()
+entry3_vars = list()
 
 contador = 0
 for concepto in concepto_list:
     concepto_opciones.append(IntVar())
-    #cajas_texto = Checkbutton(concepto_frame_lbl, text=concepto, bg=color_claro, fg=color_primario, font=("Helvetica",18), onvalue=1, offvalue=0, variable=concepto_opciones[contador], command=lambda:activar_pb(0))
-    cajas_texto = Checkbutton(concepto_frame_lbl, text=concepto, bd=2, bg=color_claro, fg=color_primario, font=("Helvetica",18), relief=FLAT, variable=concepto_opciones[contador], command=lambda:activar_textos(0))
+    cajas_texto = Checkbutton(concepto_frame_lbl, text=concepto, bd=2, bg=color_claro, fg=color_primario, font=("Helvetica",18), relief=FLAT, variable=concepto_opciones[contador], command=lambda cont=contador:activar_textos(cont))
     cajas_texto.grid(row=contador,column=0,sticky=W, padx=30, pady=5)
-    if concepto in totales_list:
-        concepto_contador_ent_1.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=23, state=DISABLED, fg=color_primario, bg=color_claro))
-        concepto_contador_ent_1[contador].grid(row=contador,column=1,columnspan=3,sticky=W)
-        entrys_a_recoger.append(concepto_contador_ent_1[contador])
-        concepto_contador_ent_2.append(Entry())
-        concepto_contador_ent_3.append(Entry())
-        txt_vars.append(0)
-        contador += 1
-    else:
-        # Primera columna
-        concepto_contador_ent_1.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
-        concepto_contador_ent_1[contador].grid(row=contador,column=1,sticky=W)
-        
-        # Variable entry del mig
-        my_var = StringVar()
-        my_var.trace("w", lambda name,index,mode,my_var=my_var,cont=contador:update_3rd_entry(my_var,cont))
-        txt_vars.append(my_var)
-        # Segona columna
-        concepto_contador_ent_2.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro, textvariable=txt_vars[contador]))
-        concepto_contador_ent_2[contador].grid(row=contador,column=2,sticky=W)
-        
-        # Tercera columna
-        concepto_contador_ent_3.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
-        concepto_contador_ent_3[contador].grid(row=contador,column=3,sticky=W)
-        entrys_a_recoger.append(concepto_contador_ent_3[contador])
-        contador += 1
+    # Primera columna
+    concepto_contador_ent_1.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro))
+    concepto_contador_ent_1[contador].grid(row=contador,column=1,sticky=W)
+    
+    # Variable entry del mig
+    entry2_var = StringVar()
+    entry2_var.trace("w", lambda name,index,mode,var=entry2_var,cont=contador:update_entry3(var,cont))
+    entry2_vars.append(entry2_var)
+    # Segona columna
+    concepto_contador_ent_2.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro, textvariable=entry2_vars[contador]))
+    concepto_contador_ent_2[contador].grid(row=contador,column=2,sticky=W)
+    
+    # Variable entry final
+    entry3_var = DoubleVar()
+    entry3_var.trace('w',lambda name,index,mode,var=entry3_var,:update_subtotal())
+    entry3_vars.append(entry3_var)
+    # Tercera columna
+    concepto_contador_ent_3.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro,textvariable=entry3_vars[contador]))
+    concepto_contador_ent_3[contador].grid(row=contador,column=3,sticky=W)
+    contador += 1
 
 # Calcular Impuestos
-# for impuestos in impuestos_list:
+## IVA
+iva_var_cb = IntVar()
+iva_var_ent = StringVar()
+iva_var_ent.trace('w',lambda name,index,mode,var=iva_var_ent,tipus="IVA":update_impost(var,tipus))
+iva_total_var = DoubleVar()
+iva_total_var.trace('w',lambda name,index,mode:update_total())
 
-#     concepto_contador_ent_1.append(Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=23, state=DISABLED, fg=color_primario, bg=color_claro))
-#     concepto_contador_ent_1[contador].grid(row=contador,column=1,columnspan=3,sticky=W)
-#     entrys_a_recoger.append(concepto_contador_ent_1[contador])
-#     concepto_contador_ent_2.append(Entry())
-#     concepto_contador_ent_3.append(Entry())
-#     txt_vars.append(0)
-#     contador += 1    
+iva_cb = Checkbutton(concepto_frame_lbl, text="IVA", bd=2, bg=color_claro, fg=color_primario, font=("Helvetica",18), relief=FLAT, variable=iva_var_cb, command=lambda:activa_impostos('IVA'))
+iva_ent = Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro, textvariable=iva_var_ent)
+iva_total_ent = Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro,textvariable=iva_total_var)
+## IRPF
+irpf_var_cb = IntVar()
+irpf_var_ent = StringVar()
+irpf_var_ent.trace('w',lambda name,index,mode,var=irpf_var_ent,tipus="IRPF":update_impost(var,tipus))
+irpf_total_var = DoubleVar()
+irpf_total_var.trace('w',lambda name,index,mode:update_total())
+
+irpf_cb = Checkbutton(concepto_frame_lbl, text="IRPF", bd=2, bg=color_claro, fg=color_primario, font=("Helvetica",18), relief=FLAT, variable=irpf_var_cb, command=lambda:activa_impostos('IRPF'))
+irpf_ent = Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro, textvariable=irpf_var_ent)
+irpf_total_ent = Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER, bd=2, width=7, state=DISABLED, fg=color_primario, bg=color_claro,textvariable=irpf_total_var)
+
+iva_cb.grid(row=6,column=0)
+iva_ent.grid(row=6,column=1,columnspan=2)
+iva_total_ent.grid(row=6,column=3)
+irpf_cb.grid(row=7,column=0)
+irpf_ent.grid(row=7,column=1,columnspan=2)
+irpf_total_ent.grid(row=7,column=3)
+
+# Calcular Subtotal y Total
+subtotal_var = DoubleVar(value=0.00)
+subtotal_var.trace('w',lambda name,index,mode:update_total())
+subtotal_lbl = Label(concepto_frame_lbl, text="Subtotal", bd=2, bg=color_claro, fg=color_primario, font=("Helvetica",18))
+subtotal_ent = Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER,state=DISABLED, bd=2, width=23, fg=color_primario, bg=color_claro,textvariable=subtotal_var)
+
+total_var = DoubleVar(value=0.00)
+total_lbl = Label(concepto_frame_lbl, text="Total", bd=2, bg=color_claro, fg=color_primario, font=("Helvetica",18))
+total_ent = Entry(concepto_frame_lbl, font=("Helvetica",18), relief=FLAT, justify=CENTER,state=DISABLED, bd=2, width=23, fg=color_primario, bg=color_claro,textvariable=total_var)
+
+subtotal_lbl.grid(row=5, column=0)
+subtotal_ent.grid(row=5, column=1,columnspan=3)
+total_lbl.grid(row=8, column=0)
+total_ent.grid(row=8, column=1,columnspan=3)
+
 
 #Botones de funciones
 pres_btn = Button(botones_frame_lbl, text="Calcular Presupuesto", font=("Helvetica",18), bd=1, padx=5, pady=5, width=36, relief=FLAT, bg=color_claro, fg=color_primario,command=calc_pressupost)
