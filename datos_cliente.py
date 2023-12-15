7493e6ee9e46f2a506c4fbacafe6faff4545da9d
Pizaguerri <p.izaguerri@gmail.com>
2023-12-15 16:52:52 +0100

    requirement.txt añadido y datos arreglados

--- datos_cliente.py
+++ datos_cliente.py
@@ -5,8 +5,8 @@ import csv
 #Abre Pop Up de Datos Cliente
 popup_cliente = Tk()
 
-color_primario = "#373737"
-color_claro = "#f8f8f8"
+COLOR_OSCURO = "#373737"
+COLOR_CLARO = "#f8f8f8"
 
 # Funciones
 ## Abre CSV Cliente y lee los datos guardados por defecto
@@ -21,25 +21,50 @@ with open("datos_cliente.csv", "r", encoding="utf-8") as datos_cliente:
 datos_clientes = ['Cliente', 'Direccion', 'CP', 'Telefono', 'Email', 'CIF']
 
 ## Genera los Entries y muestra los datos guardados por defecto, y recoge los datos de cliente sobreescritos
+# def guardar_datos_cliente(popup_cliente):
+#     entries = []
+#     with open("datos_cliente.csv", "r", encoding="utf-8") as datos_cliente:
+#         dato = csv.DictReader(datos_cliente)
+#         dict_datos_cliente = next(dato)  # Lee la primera línea del archivo CSV
+#         for datos in datos_clientes:
+#             row = Frame(popup_cliente, bd=0, padx=0, pady=1, width=5, relief=FLAT, bg=COLOR_CLARO)
+#             lab = Label(row, width=15, text=datos, anchor=W, bd=0, padx=0, pady=1, relief=FLAT, bg=COLOR_CLARO, fg=COLOR_OSCURO)
+#             ent = Entry(row,bg=COLOR_CLARO,fg=COLOR_OSCURO)
+#             valor = dict_datos_cliente.get(datos, '')  # Obtiene el valor del diccionario o un string vacío si no existe
+#             ent.insert(END, valor)
+#             row.pack(side=TOP, fill=X, padx=5, pady=5)
+#             lab.pack(side=LEFT)
+#             ent.pack(side=RIGHT, expand=YES, fill=X)
+#             entries.append((datos, ent))
+#             # Botón Guardar al final de los Entries
+#         guardar = Button(popup_cliente, text='Guardar', font=("Helvetica", 18), bd=0, padx=5, pady=5,
+#                          width=15, relief=FLAT, bg=COLOR_CLARO, fg=COLOR_OSCURO,
+#                          command=(lambda e=entries: boton_save(e, dict_datos_cliente, datos_clientes)))
+#         guardar.pack(side=BOTTOM, padx=5, pady=15)
+#     return entries
+
 def guardar_datos_cliente(popup_cliente):
     entries = []
     with open("datos_cliente.csv", "r", encoding="utf-8") as datos_cliente:
         dato = csv.DictReader(datos_cliente)
-        dict_datos_cliente = next(dato)  # Lee la primera línea del archivo CSV
+        try:
+            dict_datos_cliente = next(dato)  # Intenta leer la primera línea del archivo CSV
+        except StopIteration:
+            # Si el archivo está vacío, inicializa dict_datos_cliente como un diccionario vacío
+            dict_datos_cliente = {}
         for datos in datos_clientes:
-            row = Frame(popup_cliente, bd=0, padx=0, pady=1, width=5, relief=FLAT, bg=color_claro)
-            lab = Label(row, width=15, text=datos, anchor=W, bd=0, padx=0, pady=1, relief=FLAT, bg=color_claro, fg=color_primario)
-            ent = Entry(row,bg=color_claro,fg=color_primario)
+            row = Frame(popup_cliente, bd=0, padx=0, pady=1, width=5, relief=FLAT, bg=COLOR_CLARO)
+            lab = Label(row, width=15, text=datos, anchor=W, bd=0, padx=0, pady=1, relief=FLAT, bg=COLOR_CLARO, fg=COLOR_OSCURO)
+            ent = Entry(row, bg=COLOR_CLARO, fg=COLOR_OSCURO)
             valor = dict_datos_cliente.get(datos, '')  # Obtiene el valor del diccionario o un string vacío si no existe
             ent.insert(END, valor)
             row.pack(side=TOP, fill=X, padx=5, pady=5)
             lab.pack(side=LEFT)
             ent.pack(side=RIGHT, expand=YES, fill=X)
             entries.append((datos, ent))
-
-            # Botón Guardar al final de los Entries
+        # Botón Guardar al final de los Entries
         guardar = Button(popup_cliente, text='Guardar', font=("Helvetica", 18), bd=0, padx=5, pady=5,
-                         width=15, relief=FLAT, bg=color_claro, fg=color_primario,
+                         width=15, relief=FLAT, bg=COLOR_CLARO, fg=COLOR_OSCURO,
                          command=(lambda e=entries: boton_save(e, dict_datos_cliente, datos_clientes)))
         guardar.pack(side=BOTTOM, padx=5, pady=15)
         
@@ -61,22 +86,25 @@ def boton_save(entries, dict_datos_cliente, datos_cliente):
 
 # Ventana Principal
 popup_cliente.title("Datos de Cliente")
-popup_cliente.config(bg=color_primario)
+popup_cliente.config(bg=COLOR_OSCURO)
 popup_cliente.resizable(1,1)
 popup_cliente.minsize(625, 675)
 #popup_cliente.maxsize(825, 875)
 
 # Frames 
-popup_cliente_frame = Frame(popup_cliente,bd=20,relief=FLAT, bg=color_claro)
+popup_cliente_frame = Frame(popup_cliente,bd=20,relief=FLAT, bg=COLOR_CLARO)
 popup_cliente_frame.pack(side=BOTTOM, fill="both", expand=True, padx=10, pady=10)
 popup_cliente_frame.place(in_=popup_cliente, anchor="c", relx=.5, rely=.5)
-popup_cliente_frame_lbl = LabelFrame(popup_cliente_frame,font=("Helvetica",18),text="Introducir datos de profesional",fg=color_primario, bg=color_claro)
+popup_cliente_frame_lbl = LabelFrame(popup_cliente_frame,font=("Helvetica",18),text="Introducir datos de profesional",fg=COLOR_OSCURO, bg=COLOR_CLARO)
 
 
 # Botón Guardar guarda
 if __name__ == '__main__':
     ents = guardar_datos_cliente(popup_cliente_frame)
-    popup_cliente.bind('<Return>', (lambda e=ents: boton_save(e, dict_datos_cliente, datos_clientes)))   
+    def on_return_pressed(event, ents=ents):
+        boton_save(ents, dict_datos_cliente, datos_clientes)
+    popup_cliente.bind('<Return>', on_return_pressed)
+
 
 
 popup_cliente.mainloop()
--- datos_profesional.py
+++ datos_profesional.py
@@ -5,8 +5,8 @@ import csv
 #Abre Pop Up de Datos Profesional
 popup_prof = Tk()
 
-color_primario = "#373737"
-color_claro = "#f8f8f8"
+COLOR_OSCURO = "#373737"
+COLOR_CLARO = "#f8f8f8"
 
 # Funciones
 ## Abre CSV Profesional y lee los datos guardados por defecto
@@ -18,28 +18,31 @@ with open("datos_profesional.csv", "r", encoding="utf-8") as datos_profesional:
 #print(dict_datos_profesional)
 
 ## Lista de Labels de los Entries de Datos Profesionales
-datos_profesionales = ['Profesional', 'Direccion', 'CP', 'CIF', 'Telefono', 'Email', 'Portfolio', 'IBAN', 'SWIFT', 'Nuevo']
+datos_profesionales = ['Profesional', 'Direccion', 'CP', 'CIF', 'Telefono', 'Email', 'Portfolio', 'IBAN', 'SWIFT']
 
 ## Genera los Entries y muestra los datos guardados por defecto, y recoge los datos de profesional sobreescritos. Deja los datos en blanco si existen.
 def guardar_datos_prof(popup_prof):
     entries = []
     with open("datos_profesional.csv", "r", encoding="utf-8") as datos_profesional:
         dato = csv.DictReader(datos_profesional)
-        dict_datos_profesional = next(dato)  # Lee la primera línea del archivo CSV
+        try:
+            dict_datos_profesional = next(dato)  # Intenta leer la primera línea del archivo CSV
+        except StopIteration:
+            # Si el archivo está vacío, inicializa dict_datos_profesional como un diccionario vacío
+            dict_datos_profesional = {}
         for datos in datos_profesionales:
-            row = Frame(popup_prof, bd=0, padx=0, pady=1, width=5, relief=FLAT, bg=color_claro)
-            lab = Label(row, width=15, text=datos, anchor=W, bd=0, padx=0, pady=1, relief=FLAT, bg=color_claro, fg=color_primario)
-            ent = Entry(row,bg=color_claro,fg=color_primario)
+            row = Frame(popup_prof, bd=0, padx=0, pady=1, width=5, relief=FLAT, bg=COLOR_CLARO)
+            lab = Label(row, width=15, text=datos, anchor=W, bd=0, padx=0, pady=1, relief=FLAT, bg=COLOR_CLARO, fg=COLOR_OSCURO)
+            ent = Entry(row, bg=COLOR_CLARO, fg=COLOR_OSCURO)
             valor = dict_datos_profesional.get(datos, '')  # Obtiene el valor del diccionario o un string vacío si no existe
             ent.insert(END, valor)
             row.pack(side=TOP, fill=X, padx=5, pady=5)
             lab.pack(side=LEFT)
             ent.pack(side=RIGHT, expand=YES, fill=X)
             entries.append((datos, ent))
-
-            # Botón Guardar al final de los Entries
+        # Botón Guardar al final de los Entries
         guardar = Button(popup_prof, text='Guardar', font=("Helvetica", 18), bd=0, padx=5, pady=5,
-                         width=15, relief=FLAT, bg=color_claro, fg=color_primario,
+                         width=15, relief=FLAT, bg=COLOR_CLARO, fg=COLOR_OSCURO,
                          command=(lambda e=entries: boton_save(e, dict_datos_profesional, datos_profesionales)))
         guardar.pack(side=BOTTOM, padx=5, pady=15)
         
@@ -61,22 +64,23 @@ def boton_save(entries, dict_datos_profesional, datos_profesionales):
 
 # Ventana Principal
 popup_prof.title("Datos de Profesional")
-popup_prof.config(bg=color_primario)
+popup_prof.config(bg=COLOR_OSCURO)
 popup_prof.resizable(1,1)
 popup_prof.minsize(625, 675)
 #popup_prof.maxsize(825, 875)
 
 # Frames 
-popup_prof_frame = Frame(popup_prof,bd=20,relief=FLAT, bg=color_claro)
+popup_prof_frame = Frame(popup_prof,bd=20,relief=FLAT, bg=COLOR_CLARO)
 popup_prof_frame.pack(side=BOTTOM, fill="both", expand=True, padx=10, pady=10)
 popup_prof_frame.place(in_=popup_prof, anchor="c", relx=.5, rely=.5)
-popup_prof_frame_lbl = LabelFrame(popup_prof_frame,font=("Helvetica",18),text="Introducir datos de profesional",fg=color_primario, bg=color_claro)
+popup_prof_frame_lbl = LabelFrame(popup_prof_frame,font=("Helvetica",18),text="Introducir datos de profesional",fg=COLOR_OSCURO, bg=COLOR_CLARO)
 
 
 # Botón Guardar guarda
 if __name__ == '__main__':
     ents = guardar_datos_prof(popup_prof_frame)
-    popup_prof.bind('<Return>', (lambda e=ents: boton_save(e, dict_datos_profesional, datos_profesionales)))   
-
+    def on_return_pressed(event, ents=ents):
+        boton_save(ents, dict_datos_profesional, datos_profesionales)
+    popup_prof.bind('<Return>', on_return_pressed)
 
 popup_prof.mainloop()
